44f6b745cb88eb6276e9858fdb1557149f11561e
Marco <marcolivier.rodrigue@gmail.com>
2023-05-31 11:03:51 -0400

    Better test for isPerfectSquare method

--- test/src/Util_test.cpp
+++ test/src/Util_test.cpp
@@ -1,11 +1,37 @@
 #include "Util.h"
 #include "gtest/gtest.h"
 #include <limits.h>
+#include <set>
+#include <vector>
 
-TEST(Util, handleNegative)
+TEST(Util, isPerctSquare)
+{
+    bool res = true;
+    std::vector<int> perfectSquares = {
+        0,1,4,9,16,
+        25,36,49,64,81,
+        100,121,144,169,196,
+        225,256,289,324,361,
+        400,441,484,529,576,
+        625,676,729,784,841,
+        900,961,1024
+    };
+    std::set<int>perfectSquareSet(perfectSquares.begin(), perfectSquares.end());
+
+    for(int i = 0; i <= perfectSquares[perfectSquares.size()-1]; ++i)
+    {
+        if(isPerfectSquare(i) != perfectSquareSet.count(i))
+        {
+            res = false; 
+            break;
+        }
+    }
+    EXPECT_TRUE(res);
+}
+
+TEST(Util, isPerfectSquare_handleNegative)
 {
     EXPECT_FALSE(isPerfectSquare(-9));
-    EXPECT_TRUE(isPerfectSquare(9));
 }
 
 TEST(Util, selectRandomNum)
